20605d08fed638e5ff5e30fdde5c414302d18a25
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2023-10-04 20:27:38 -0300

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Campanhas do Sangue';
+export const SITE_DESCRIPTION = 'Esse projeto visa a distribuição gratuita de informação e campanhas para os Hemocentros do Brasil. Por não ser um projeto tão visado, ele está sendo hosteado gratuitamente através do Github Pages.';
