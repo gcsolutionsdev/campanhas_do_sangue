1e458a3fec02a99ee8695cb58c4b9f8909426c99
Paulo Santana <paulosantana.sa@gmail.com>
2023-10-05 20:02:13 -0300

    feat: Make RSS feed distribution dynamic
    Implemented dynamic RSS feed distribution to adapt to changing content.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,35 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import * as fs from "node:fs";
+import path from "node:path";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+
+const __dirname = process.cwd();
+const contentDir = path.join(__dirname, "src", "content");
+
+const collectionNames = fs.readdirSync(contentDir).filter((item) => {
+  const itemPath = path.join(contentDir, item);
+  const stats = fs.statSync(itemPath);
+  return stats.isDirectory();
+});
 
 export async function GET(context) {
-	const posts = await getCollection('hemoce');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/campanhas_do_sangue/hemoce/${post.slug}/`,
-		})),
-	});
+  const allPosts = [];
+
+  for (const collectionName of collectionNames) {
+    const posts = await getCollection(collectionName);
+    allPosts.push(...posts);
+  }
+
+  const rssFeed = rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: allPosts.map((post) => ({
+      ...post.data,
+      link: `/campanhas_do_sangue/${post.collection}/${post.slug}/`,
+    })),
+  });
+
+  return rssFeed;
 }
