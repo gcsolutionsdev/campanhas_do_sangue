7a5573fd947ac838be90381e827fd763381d50a6
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2023-10-05 11:52:09 -0300

    feat: HEMOCE:: Nota de repúdio

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const hemoce = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		favicon: z.string().optional()
 	}),
 });
 
-export const collections = { blog };
+export const collections = { hemoce };
